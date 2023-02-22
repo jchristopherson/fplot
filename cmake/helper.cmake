ad51bced37856fd5afac06ac2df0c94f6c2bf2c2
jchristopherson <jchristopherson@hotmail.com>
2023-02-22 15:05:52 -0600

    Clean up build scripts

--- cmake/helper.cmake
+++ cmake/helper.cmake
@@ -59,7 +59,7 @@ endfunction()
 
 # Links the supplied library
 function(link_library targ lib include_dir)
-    target_link_libraries(${targ} ${lib})
+    target_link_libraries(${targ} PRIVATE ${lib})
     target_include_directories(${targ} PUBLIC $<BUILD_INTERFACE:${include_dir}>)
 endfunction()
 
