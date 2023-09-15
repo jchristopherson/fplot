7295cf29c625c52a3221ebd041ac87f3737bcb10
jchristopherson <jchristopherson@hotmail.com>
2023-09-15 11:37:50 -0500

    Update comments

--- test/fplot_test.f90
+++ test/fplot_test.f90
@@ -5,7 +5,8 @@ program main
     ! Local Variables
     logical :: check
 
-    ! Tests
+    ! Tests - Started on Sept. 15, 2023.
+    ! TO DO: Add tests as this project evolves.
     check = test_3d_coordinate_system()
     if (.not.check) stop 1
 end program
