223fc6371ad2df7bd25c2c9bf71feb7dbce509f5
jchristopherson <jchristopherson@hotmail.com>
2025-03-28 14:49:48 -0500

    Add alpha channel to color type

--- src/fplot_colors.f90
+++ src/fplot_colors.f90
@@ -34,6 +34,9 @@ module fplot_colors
             !! The green component of the color (must be between 0 and 255).
         integer(int32), public :: blue = 255
             !! The blue component of the color (must be between 0 and 255).
+        integer(int32), public :: alpha = 0
+            !! The alpha component of the color (must be between 0 and 255).
+            !! Notice, 0 is fully opaque and 255 is fully transparent.
     contains
         procedure, public, pass :: to_hex_string => clr_to_hex_string
         procedure, public, pass :: copy_from => clr_copy_from
@@ -47,50 +50,50 @@ module fplot_colors
         module procedure :: clr_not_equals
     end interface
 
-    type(color), parameter :: CLR_BLACK = color(0, 0, 0)
+    type(color), parameter :: CLR_BLACK = color(0, 0, 0, 0)
         !! Black.
-    type(color), parameter :: CLR_WHITE = color(255, 255, 255)
+    type(color), parameter :: CLR_WHITE = color(255, 255, 255, 0)
         !! White.
-    type(color), parameter :: CLR_RED = color(255, 0, 0)
+    type(color), parameter :: CLR_RED = color(255, 0, 0, 0)
         !! Red.
-    type(color), parameter :: CLR_LIME = color(0, 255, 0)
+    type(color), parameter :: CLR_LIME = color(0, 255, 0, 0)
         !! Lime.
-    type(color), parameter :: CLR_BLUE = color(0, 0, 255)
+    type(color), parameter :: CLR_BLUE = color(0, 0, 255, 0)
         !! Blue.
-    type(color), parameter :: CLR_YELLOW = color(255, 255, 0)
+    type(color), parameter :: CLR_YELLOW = color(255, 255, 0, 0)
         !! Yellow.
-    type(color), parameter :: CLR_CYAN = color(0, 255, 255)
+    type(color), parameter :: CLR_CYAN = color(0, 255, 255, 0)
         !! Cyan.
-    type(color), parameter :: CLR_MAGENTA = color(255, 0, 255)
+    type(color), parameter :: CLR_MAGENTA = color(255, 0, 255, 0)
         !! Magenta.
-    type(color), parameter :: CLR_SILVER = color(192, 192, 192)
+    type(color), parameter :: CLR_SILVER = color(192, 192, 192, 0)
         !! Silver.
-    type(color), parameter :: CLR_GRAY = color(128, 128, 128)
+    type(color), parameter :: CLR_GRAY = color(128, 128, 128, 0)
         !! Gray.
-    type(color), parameter :: CLR_MAROON = color(128, 0, 0)
+    type(color), parameter :: CLR_MAROON = color(128, 0, 0, 0)
         !! Maroon.
-    type(color), parameter :: CLR_OLIVE = color(128, 128, 0)
+    type(color), parameter :: CLR_OLIVE = color(128, 128, 0, 0)
         !! Olive.
-    type(color), parameter :: CLR_GREEN = color(0, 128, 0)
+    type(color), parameter :: CLR_GREEN = color(0, 128, 0, 0)
         !! Green.
-    type(color), parameter :: CLR_PURPLE = color(128, 0, 128)
+    type(color), parameter :: CLR_PURPLE = color(128, 0, 128, 0)
         !! Purple.
-    type(color), parameter :: CLR_TEAL = color(0, 128, 128)
+    type(color), parameter :: CLR_TEAL = color(0, 128, 128, 0)
         !! Teal.
-    type(color), parameter :: CLR_NAVY = color(0, 0, 128)
+    type(color), parameter :: CLR_NAVY = color(0, 0, 128, 0)
         !! Navy.
-    type(color), parameter :: CLR_ORANGE = color(255, 165, 0)
+    type(color), parameter :: CLR_ORANGE = color(255, 165, 0, 0)
         !! Orange.
 
     ! A list of colors that can be cycled through by plotting code
     type(color), parameter, dimension(7) :: color_list = [ &
-        color(0, int(0.447 * 255), int(0.741 * 255)), &
-        color(int(0.85 * 255), int(0.325 * 255), int(0.098 * 255)), &
-        color(int(0.929 * 255), int(0.694 * 255), int(0.125 * 255)), &
-        color(int(0.494 * 255), int(0.184 * 255), int(0.556 * 255)), &
-        color(int(0.466 * 255), int(0.674 * 255), int(0.188 * 255)), &
-        color(int(0.301 * 255), int(0.745 * 255), int(0.933 * 255)), &
-        color(int(0.635 * 255), int(0.078 * 255), int(0.184 * 255))]
+        color(0, int(0.447 * 255), int(0.741 * 255), 0), &
+        color(int(0.85 * 255), int(0.325 * 255), int(0.098 * 255), 0), &
+        color(int(0.929 * 255), int(0.694 * 255), int(0.125 * 255), 0), &
+        color(int(0.494 * 255), int(0.184 * 255), int(0.556 * 255), 0), &
+        color(int(0.466 * 255), int(0.674 * 255), int(0.188 * 255), 0), &
+        color(int(0.301 * 255), int(0.745 * 255), int(0.933 * 255), 0), &
+        color(int(0.635 * 255), int(0.078 * 255), int(0.184 * 255), 0)]
 
 contains
 ! ------------------------------------------------------------------------------
@@ -98,11 +101,11 @@ contains
         !! Returns the color in hexadecimal format.
         class(color), intent(in) :: this
             !! The color object.
-        character(6) :: txt
+        character(8) :: txt
             !! A string containing the hexadecimal equivalent.
 
         ! Local Variables
-        integer(int32) :: r, g, b, clr
+        integer(int32) :: r, g, b, a, clr
 
         ! Clip each color if necessary
         if (this%red < 0) then
@@ -129,11 +132,19 @@ contains
             b = this%blue
         end if
 
+        if (this%alpha < 0) then
+            a = 0
+        else if (this%alpha > 255) then
+            a = 255
+        else
+            a = this%alpha
+        end if
+
         ! Build the color information
-        clr = ishft(r, 16) + ishft(g, 8) + b
+        clr = ishft(a, 24) + ishft(r, 16) + ishft(g, 8) + b
 
         ! Convert the integer to a hexadecimal string
-        write(txt, '(Z6.6)') clr
+        write(txt, '(Z8.8)') clr
     end function
 
 ! ------------------------------------------------------------------------------
