c7367b7dbc8b249461ec4654c74e76c61522105e
jchristopherson <jchristopherson@hotmail.com>
2025-04-07 14:26:50 -0500

    Update example

--- examples/histogram_example.f90
+++ examples/histogram_example.f90
@@ -9,9 +9,11 @@ program example
     real(real64) :: x(n), u(n), v(n)
     type(plot_bar) :: plt   ! can also be plot_2d
     type(plot_data_histogram) :: pd1
+    class(plot_axis), pointer :: xAxis
 
     ! Initialization
     call plt%initialize()
+    xAxis => plt%get_x_axis()
 
     ! Create some data
     call random_number(u)
@@ -19,8 +21,12 @@ program example
     v = v - 1.0d0
     x = u * u - v * v
 
+    ! Rotate the labels on the x axis
+    call xAxis%set_tic_label_angle(90.0)
+    call xAxis%set_tic_label_rotation_origin(GNUPLOT_ROTATION_ORIGIN_RIGHT)
+
     ! Plot the data
-    call pd1%set_bin_count(nbins)   ! optiona, but must be done prior to define_data is used
+    call pd1%set_bin_count(nbins)   ! optional, but must be done prior to define_data is used
     call pd1%define_data(x)
     call pd1%set_transparency(0.5)  ! optional - for illustration purposes
     call plt%push(pd1)
