a0aeba867e3ed22a8d3acc5eb8b02108a8627178
Jason Christopherson <jchristopherson@hotmail.com>
2026-06-11 06:02:17 -0500

    Add to box and whisker

--- examples/box_whisker_example.f90
+++ examples/box_whisker_example.f90
@@ -5,7 +5,7 @@ program example
     implicit none
 
     ! Variables
-    integer(int32), parameter :: n = 5
+    integer(int32), parameter :: n = 3
     integer(int32) :: i
     type(string) :: titles(n)
     real(real64) :: boxmin(n), boxmax(n), whiskermin(n), whiskermax(n)
@@ -14,22 +14,18 @@ program example
 
     ! Initialization
     do i = 1, n
-        titles(i) = "Item " // to_string(i)
+        titles(i) = "Item-" // to_string(i)
     end do
-    call random_number(whiskermin)
-    call random_number(whiskermax)
-    call random_number(boxmin)
-    call random_number(boxmax)
-
-    ! Ensure proper order
-    boxmin = boxmin + whiskermin
-    boxmax = boxmin + boxmax
-    whiskermax = whiskermax + boxmax
+    boxmin = [4.5d0, 5.0d0, 6.0d0]
+    boxmax = [7.5d0, 8.0d0, 9.0d0]
+    whiskermin = [3.0d0, 4.0d0, 5.0d0]
+    whiskermax = [9.0d0, 10.0d0, 11.0d0]
 
     ! Create the plot
     call plt%initialize()
 
     call pd%define_data(titles, boxmin, boxmax, whiskermin, whiskermax)
+    call pd%set_line_width(2.0)
     call plt%push(pd)
     call plt%draw()
 end program
--- src/fplot_plot_data_box_whisker.f90
+++ src/fplot_plot_data_box_whisker.f90
@@ -23,12 +23,28 @@ module fplot_plot_data_box_whisker
             !! The maximum y-values for each whisker.
         logical, private :: m_useY2 = .false.
             !! Plot against the secondary y-axis?
+        logical, private :: m_whiskerbars = .true.
+            !! Use horizontal whisker bar caps?
+        real(real32), private :: m_whiskerWidth = 1.0
+            !! On a scale of 0 -> 1, the whiskerwidth.
+        real(real32), private :: m_lineWidth = 1.0
+            !! The line width.
+        real(real32), private :: m_boxWidth = 0.05
+            !! The box width.
     contains
         procedure, public :: define_data => pdbw_define_data_xstring
         procedure, public :: get_command_string => pdbw_get_cmd
         procedure, public :: get_data_string => pdbw_get_data_cmd
         procedure, public :: get_draw_against_y2 => pdbw_get_use_y2
         procedure, public :: set_draw_against_y2 => pdbw_set_use_y2
+        procedure, public :: get_use_whiskerbars => pdbw_get_use_whiskerbars
+        procedure, public :: set_use_whiskerbars => pdbw_set_use_whiskerbars
+        procedure, public :: get_whiskerbar_width => pdbw_get_whiskerbar_width
+        procedure, public :: set_whiskerbar_width => pdbw_set_whiskerbar_width
+        procedure, public :: get_line_width => pdbw_get_line_width
+        procedure, public :: set_line_width => pdbw_set_line_width
+        procedure, public :: get_box_width => pdbw_get_box_width
+        procedure, public :: set_box_width => pdbw_set_box_width
     end type
 
 contains
@@ -95,26 +111,37 @@ function pdbw_get_cmd(this) result(rst)
     integer(int32) :: n, nname
     type(color) :: clr
 
+    ! Style
+    ! call str%append(' "-" using ($0+1):2:3:4:5:xtic(1) with candlesticks')
+    call str%append(' "-" using ($0+1):2:3:4:5:(')
+    call str%append(to_string(this%get_box_width()))
+    call str%append("):xtic(1) with candlesticks")
+
     ! Title
     nname = len_trim(this%get_name())
     if (n > 0) then
-        call str%append(' "-" title "')
+        call str%append(' title "')
         call str%append(this%get_name())
         call str%append('"')
     else
-        call str%append(' "-" notitle')
+        call str%append(' notitle')
     end if
 
-    ! Style
-    call str%append(" using ($0+1):2:3:4:5:xtic(1) with candlesticks")
-
     ! Whisker bars
+    if (this%get_use_whiskerbars()) then
+        call str%append(" whiskerbars ")
+        call str%append(to_string(this%get_whiskerbar_width()))
+    end if
 
     ! Color
     clr = this%get_line_color()
     call str%append(' lc rgb "#')
     call str%append(clr%to_hex_string())
     call str%append('"')
+
+    ! Line Width
+    call str%append(" lw ")
+    call str%append(to_string(this%get_line_width()))
 
     ! End
     rst = char(str%to_string())
@@ -198,8 +225,101 @@ subroutine pdbw_set_use_y2(this, x)
 end subroutine
 
 ! ------------------------------------------------------------------------------
+pure function pdbw_get_use_whiskerbars(this) result(rst)
+    !! Gets a value determining if whiskerbars should be used.
+    class(plot_data_box_whisker), intent(in) :: this
+        !! The plot_data_box_whisker object.
+    logical :: rst
+        !! True if whiskerbars should be used; else, false.
+    rst = this%m_whiskerbars
+end function
+
+! --------------------
+subroutine pdbw_set_use_whiskerbars(this, x)
+    !! Sets a value determining if whiskerbars should be used.
+    class(plot_data_box_whisker), intent(inout) :: this
+        !! The plot_data_box_whisker object.
+    logical, intent(in) :: x
+        !! Set to true if whiskerbars should be used; else, false.
+    this%m_whiskerbars = x
+end subroutine
 
 ! ------------------------------------------------------------------------------
+pure function pdbw_get_whiskerbar_width(this) result(rst)
+    !! Gets the width of whiskerbar.
+    class(plot_data_box_whisker), intent(in) :: this
+        !! The plot_data_box_whisker object.
+    real(real32) :: rst
+        !! The width of the whiskerbar on a scale of 0:1 with 1 being the full
+        !! width.
+    rst = this%m_whiskerWidth
+end function
+
+! --------------------
+subroutine pdbw_set_whiskerbar_width(this, x)
+    !! Sets the width of the whiskerbar.
+    class(plot_data_box_whisker), intent(inout) :: this
+        !! The plot_data_box_whisker object.
+    real(real32), intent(in) :: x
+        !! The width of the whiskerbar.  This value is clamped to [0, 1] with
+        !! 1 representing full width.
+
+    if (x < 0.0d0) then
+        this%m_whiskerWidth = 0.0d0
+    else if (x > 1.0d0) then
+        this%m_whiskerWidth = 1.0d0
+    else
+        this%m_whiskerWidth = x
+    end if
+end subroutine
+
+! ------------------------------------------------------------------------------
+pure function pdbw_get_line_width(this) result(x)
+    !! Gets the width of the line, in pixels.
+    class(plot_data_box_whisker), intent(in) :: this
+        !! The plot_data_box_whisker object.
+    real(real32) :: x
+        !! The line width.
+    x = this%m_lineWidth
+end function
+
+! --------------------
+subroutine pdbw_set_line_width(this, x)
+    !! Sets the width of the line, in pixels.
+    class(plot_data_box_whisker), intent(inout) :: this
+        !! The plot_data_box_whisker object.
+    real(real32), intent(in) :: x
+        !! The line width.
+    this%m_lineWidth = x
+end subroutine
+
+! ------------------------------------------------------------------------------
+pure function pdbw_get_box_width(this) result(rst)
+    !! Gets the box width.
+    class(plot_data_box_whisker), intent(in) :: this
+        !! The plot_data_box_whisker object.
+    real(real32) :: rst
+        !! The box width.
+    rst = this%m_boxWidth
+end function
+
+! --------------------
+subroutine pdbw_set_box_width(this, x)
+    !! Sets the box width.
+    class(plot_data_box_whisker), intent(inout) :: this
+        !! The plot_data_box_whisker object.
+    real(real32), intent(in) :: x
+        !! The box width.
+    this%m_boxWidth = x
+end subroutine
+
+! ------------------------------------------------------------------------------
+
+! --------------------
+
+! ------------------------------------------------------------------------------
+
+! --------------------
 
 ! ------------------------------------------------------------------------------
 end module
